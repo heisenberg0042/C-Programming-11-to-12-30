9f59453d35f752d16315df88b94ced242c503985
heisenberg0042 <94596495+heisenberg0042@users.noreply.github.com>
2021-12-11 12:45:11 +0530

    Add files via upload

---
+++ C++/FirstProgramC++.cpp
@@ -0,0 +1,33 @@
+// c++ programming language
+
+// c programming  // procedure oriented programming language
+
+// c++ // object oriendted programming   // oopc
+
+// oopc
+
+// class // object // inheritance // polymorphism // abstraction // encapsulation
+
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+main(){
+	
+	int x =23;
+	int y =33;
+	
+	float z;
+	
+	cout << "Hello World"<<endl;
+	
+	cout << x << " and " << y << endl ;
+	
+	cout<<"Enter Number"<<endl;
+	
+	cin >> z;
+	
+	cout<<"Value that u enterd is "<< fixed << setprecision(2) << z <<endl;
+	
+}
